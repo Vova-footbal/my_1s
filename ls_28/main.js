4ffbbf5a56da725ac82788c6b405e7dcd70969ec
Vova-footbal <74201081+Vova-footbal@users.noreply.github.com>
2021-03-13 19:47:22 +0200

    Add files via upload

---
+++ ls_28/main.js
@@ -0,0 +1,33 @@
+// // let x = 256
+// // let y = 128
+// // let z = 64
+// let x = 35
+//     y = 654
+//     z = 674
+//     c = x + z + y
+//     b = x - z - y
+//     n = x * z * y 
+//     r = x / z / y
+//     h = ""
+// // document.write(c)
+// console.log(c)
+// console.log(b)
+// console.log(n)
+// console.log(r)
+// document.write(h)
+// console.log(h)
+// alert("")
+ let r = prompt("Как тебя зовут?");
+ alert(r + "Hi")
+ let e = prompt("Привет, " + r + " сколько тебе лет?" );
+ let w = prompt("Твой тлюбимый цвет?");
+ let q = prompt("В каком ты классе?");
+ let f = prompt("sdfsdf?");
+ console.log(r)
+ console.log(e)
+ console.log(w)
+ console.log(q)
+ console.log(f)
+
+
+
