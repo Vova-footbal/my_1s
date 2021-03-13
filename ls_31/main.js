d8b04b18d5ae1751ab7c08d5ca67d2190c279d66
Vova-footbal <74201081+Vova-footbal@users.noreply.github.com>
2021-03-13 19:47:56 +0200

    Add files via upload

---
+++ ls_31/main.js
@@ -0,0 +1,25 @@
+let
+x = Number (prompt("Введи первое число"))
+y = Number (prompt("Введи второе число"))
+console.log 
+alert ("Сумма " +(x+y))
+        // +(x-y)
+        // +(x*y)
+        // +(x/y)
+        // +(x^y))
+x = Number (prompt("Введи первое число для вычитания"))
+y = Number (prompt("Введи второе число для вычитания"))
+console.log 
+alert ("Разность " +(x-y))
+x = Number (prompt("Введи первое число для умножения"))
+y = Number (prompt("Введи второе число для умножения"))
+console.log 
+alert ("Умножение " +(x*y))
+x = Number (prompt("Введи первое число для деления"))
+y = Number (prompt("Введи второе число для деления"))
+console.log 
+alert ("Деление " +(x/y))
+x = Number (prompt("Введи первое число для возведения в степень"))
+y = Number (prompt("Введи степень числа"))
+console.log 
+alert ("степень " +(x^y))
