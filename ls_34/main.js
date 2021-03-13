e22b304950e45c94075b94241de896124ad664ba
Vova-footbal <74201081+Vova-footbal@users.noreply.github.com>
2021-03-13 19:48:38 +0200

    Add files via upload

---
+++ ls_34/main.js
@@ -0,0 +1,152 @@
+// let a= Number(prompt("Любое число a "))
+//     b= Number(prompt("Любое число b"))
+//     c= Number(prompt("Любое число c"))
+//     d= Number(prompt("Любое число d"))
+// if(a > c){
+//     console.log("a > c")
+//     console.log("Net")
+// }
+// else if(a < c){
+//     console.log("a < c")
+//     console.log("Da")
+// }
+// else if(b > d) {
+//     console.log("b > d")
+//     console.log("Net")
+// }
+// else if(b < d){
+//     console.log("b < d")
+//     console.log("")
+// }
+
+// b= Number(prompt("Любое число b"))
+// if(b > c){
+//     alert("Да верно")
+// }
+// else if(b < c){
+//     alert("Не верно")
+// }
+// else if(b == c) {
+//     alert("Не верно")
+// }
+// else {
+//     alert("Ну и что это")
+// }
+
+
+
+// if(c > c){
+//     alert("не верно")
+// }
+// else if(c < c){
+//     alert("Не верно")
+// }
+// else if(с == c) {
+//     alert("Верно")
+// }
+// else {
+//     alert("Ну и что это")
+// }
+
+
+// if(d > c){
+//     alert("Не верно")
+// }
+// else if(d < c){
+//     alert("Верно")
+// }
+// else if(d == c) {
+//     alert("Не верно")
+// }
+// else {
+//     alert("Ну и что это")
+// }
+
+
+// if(a > b){
+//     alert("Да верно")
+// }
+// else if(a < b){
+//     alert("Не верно")
+// }
+// else if(a == b) {
+//     alert("Не верно")
+// }
+// else {
+//     alert("Ну и что это")
+// }
+
+
+// if(b > b){
+//     alert("Да верно")
+// }
+// else if(b < b){
+//     alert("Не верно")
+// }
+// else if(b == b) {
+//     alert("Не верно")
+// }
+// else {
+//     alert("Ну и что это")
+// }
+
+
+
+// if(c > b){
+//     alert("не верно")
+// }
+// else if(c < b){
+//     alert("Не верно")
+// }
+// else if(c == b) {
+//     alert("Верно")
+// }
+// else {
+//     alert("Ну и что это")
+// }
+
+
+
+
+// if(d > b){
+//     alert("Не верно")
+// }
+// else if(d < b){
+//     alert("Верно")
+// }
+// else if(d == b) {
+//     alert("Не верно")
+// }
+// else {
+//     alert("Ну и что это")
+// }
+
+// let x = prompt(" Равнобедренный треугольник")
+// if (x == "Да"){
+//     alert("2 стороны равны")
+// }
+// else{
+//     alert(" Разностороний")
+// }
+// let t = prompt("Кто ты?")
+// if(t == "Администратор"){
+//     alert("Здравствуйте")
+//     let x = prompt("Введите пароль")
+//     console.log (x)
+// }
+// else if(t == "Пользователь"){
+//     alert("Дарова чел без подписки")
+// }
+// else if(t == "Подписчик"){
+//     alert("Добрый день много уважаем")
+// }
+let a = prompt("Решим задачу?")
+    if (a == "Да"){
+   alert("Тогда поедем домой")
+}
+else if(a == "За 5 минут успеем ли мы?"){
+    alert("Нам даст Денис")
+}
+else {
+    alert("Остаемся до скольки?")
+}
