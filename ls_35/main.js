6dc76f2a9cb8d30f380f3dd397e79041ceb8b7e4
Vova-footbal <74201081+Vova-footbal@users.noreply.github.com>
2021-03-13 19:49:33 +0200

    Add files via upload

---
+++ ls_35/main.js
@@ -0,0 +1,53 @@
+// let bool = 1 || 0
+//     a = 1 || 1
+//     b = 0 || 1
+//     c = 0 || 0
+//     alert(bool)
+//     alert(a)
+//     alert(b)
+//     alert(c)
+// let a =prompt("Время работы магазина")
+// if(a < 10 || a > 18){
+//     alert("Закрыто")
+// } 
+// else{
+//     alert("Открыто")
+// }
+// let bool = 1 && 0
+//     a = 1 && 1
+//     b = 0 && 1
+//     c = 0 && 0
+//     alert(bool)
+//     alert(a)
+//     alert(b)
+//     alert(c)
+// alert(1 && 0)
+// alert(1 && 5)
+// alert(null && 5)
+// alert(1 && "voobche")
+// alert(1 && 2 && 3)
+// let x=!1
+// alert(x)
+// alert(!"vova")
+// alert(!!null)
+// alert(alert (1) && 2 || alert (3))
+// alert(null || 2 && 3 || 4)
+let a =prompt("Кто ты?")
+if ( a == "Админ"){
+    let b =("Пароль")
+    if(b== 1905){
+    alert("Вы вошли в акаунт")
+    }    
+    else{
+        alert("Вы не вошли в акаунт")
+    }
+    else if{
+        alert("Действия отменены")
+    }
+}
+else if( !a == "Админ"){
+    alert("Тогда кто ты?")
+}
+else{
+    alert("Ты что-то инное")
+}
