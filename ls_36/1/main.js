31607add012d5b6d78b12b735514c25fdd34a332
Vova-footbal <74201081+Vova-footbal@users.noreply.github.com>
2021-03-18 20:17:49 +0200

    Add files via upload

---
+++ ls_36/1/main.js
@@ -0,0 +1,40 @@
+// let a =prompt("Введите число")
+// if(a == 10){
+//     alert("Верно")
+// }
+// else{
+//     alert("Не верно")
+// }
+// let min = Number(prompt("Число"))
+// if(min >= 0 && min <= 15){
+//     alert("Первая четверть")
+// }
+// else if(min >= 16 && min <= 30){
+//     alert("Вторая четверть")
+// }
+// else if(min >= 31 && min <= 45){
+//     alert("Третья четверть ")
+// }
+// else if(min >= 46 && min <= 60){
+//     alert("Четвертая четверть")
+// }
+// else{
+//     alert("Это лишнее")
+// }
+// let lang =prompt("Language?")
+// if(lang == "ru"){
+//     alert("Понедельник,Вторник,Среда,Четверг,Пятница")
+// }
+// if(lang == "en"){
+//     alert("monday,tuesday,wednesday,thursday,friday")
+// }
+// let arr = ["Пн"," Вт"," Ср","Пт"]
+// alert(arr[4])
+// let a =Number(prompt("Введите a"))
+//     b =Number(prompt("Введите b"))
+// if(a <= 1 && b >= 3){
+//     alert(a+b)
+// }
+// else{
+//     alert(a-b)
+// }
