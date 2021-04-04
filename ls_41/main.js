5930517706d48187a1ce38e45da6f8c33b588e91
Vova-footbal <74201081+Vova-footbal@users.noreply.github.com>
2021-04-04 15:00:16 +0300

    Add files via upload

---
+++ ls_41/main.js
@@ -0,0 +1,136 @@
+// let a = Number(prompt("число"))
+// switch(a){
+//     case 0 :alert("YES sir");
+// break;
+// default:
+//     alert("NO sir")
+// ;
+// }
+// let a = Number(prompt("число"))
+// switch(a==0){
+//     case true :
+//         alert("YES sir");
+// break;
+// default: 
+//     alert("NO sir")
+// ;
+// }
+// let a = Number(prompt("число"))
+// switch(a > 0){
+//     case true:
+//         alert("true");
+//     break;
+//     default:
+//         alert("lie");
+// }
+// let a = Number(prompt("число"))
+// switch(a < 0){
+//     case true:
+//         alert("true");
+//     break;
+//     default:
+//         alert("lie");
+// }
+// let a = Number(prompt("число"))
+// switch(a >= 0){
+//     case true:
+//         alert("true");
+//     break;
+//     default:
+//         alert("lie");
+// }
+// let a = Number(prompt("число"))
+// switch(a <= 0){
+//     case true:
+//         alert("true");
+//     break;
+//     default:
+//         alert("lie");
+// }
+// let a = Number(prompt("число"))
+// switch(a != 0){
+//     case true:
+//         alert("true");
+//     break;
+//     default:
+//         alert("lie");
+// }
+// let a = (prompt("test"))
+// switch(a = "test"){
+//     case true:
+//         alert("true");
+//      break;
+//      default:
+//          alert("lie");
+// }
+// let a = prompt("число")
+// switch(a == "'1'" || a == 1){
+//     case true:
+//         alert("true");
+//     break;
+//     default:
+//         alert("lie");
+// }
+//
+// let a = Number(prompt("число"))
+// if(a == 0){
+//     alert("true")
+// }
+// else(
+//     alert("lie")
+// )
+// }
+// let a = Number(prompt("число"))
+// if(a > 0){
+//     alert("true")
+// }
+// else(
+//     alert("lie")
+// )
+// }
+// let a = Number(prompt("число"))
+// if(a < 0){
+//     alert("true")
+// }
+// else(
+//     alert("lie")
+// )
+// }
+// let a = Number(prompt("число"))
+// if(a =< 0){
+//     alert("true")
+// }
+// else(
+//     alert("lie")
+// )
+//}
+// let a = Number(prompt("число"))
+// if(a >= 0){
+//     alert("true")
+// }
+// else(
+//     alert("lie")
+// )
+//}
+// let a = Number(prompt("число"))
+// if(a != 0){
+//     alert("true")
+// }
+// else(
+//     alert("lie")
+// )
+//}
+// let a = Number(prompt("test"))
+// if(a == "test"){
+//     alert("true")
+// }
+// else(
+//     alert("lie")
+// )
+//}
+// let a = prompt("число")
+// if(a == "'1'" || a == 1){
+//         alert("true");}
+// else(
+//    alert("lie")
+//)
