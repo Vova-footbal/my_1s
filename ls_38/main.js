8ad6ac5a0c8d9ef7e719aa2f20c57aef2381fc75
Vova-footbal <74201081+Vova-footbal@users.noreply.github.com>
2021-04-11 11:18:36 +0300

    Add files via upload

---
+++ ls_38/main.js
@@ -0,0 +1,73 @@
+// let a =Number(prompt("Введите число"))
+// if (a>0){
+//     a++
+//     alert(a)
+// }
+// else{
+//     alert(a)
+// }
+// let a = Number(prompt("Введите число"))
+// if (a>0){
+//     a++
+//     alert(a)
+// }
+// else{
+//     alert(a-2)
+// }
+// let a = Number(prompt("Введите число"))
+// if (a>0){
+//     a++
+//     alert(a)
+// }
+// else if(a<0){
+//     alert(a-2)
+// }
+// else if(a==0){
+//     alert(a+10)
+// }
+// else{
+//     alert("инное")
+// }
+// let a = Number(prompt("Введите число"))
+//     b = Number(prompt("Введите число"))
+//     с = Number(prompt("Введите число"))
+// if (a>0 && b>0 && c>0){
+//     alert(3)
+// }
+// else if (a<0 && b<0 && c<0){
+//     alert(2)
+// }
+// else if(a<0 && b>0 && c>0){
+//     alert(1)
+// }
+// else if (a<0 && b>0 && c>0){
+//     alert(4)
+// }
+// else if(a>0 && b<0 && c>0){
+//     alert(5)
+// }
+// else if (a>0 && b>0 && c<0){
+//     alert(6)
+// }
+// else if(a<0 && b<0 && c>0){
+//     alert(7)
+// }
+// else if(a<0 && b<0 && c<0){
+//     alert(8)
+// }
+// else if(a<0 && b>0 && c>0){
+//     alert(9)
+// }
+
+
+
+
+
+// let a = prompt("Введите 1 число")
+//     b = prompt("Введите 2 число")
+// if(a > b){
+//     alert(a)
+// }
+// else{
+//     alert(b)
+// }
