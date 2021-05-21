48875da5ee8d0aebd5d41a4e60fb5ea2ddc1c1f7
Vova-footbal <74201081+Vova-footbal@users.noreply.github.com>
2021-05-21 16:21:19 +0300

    Add files via upload

---
+++ ls_48/main.js
@@ -0,0 +1,10 @@
+let a = document.getElementById("text5");
+// b = a.innerHTML;
+// a.innerHTML = "Здесь был параграф 3";
+// b = b + "A";
+// alert(b);
+// a.innerHTML = "Ты готов?"
+// console.log(a.innerHTML);
+// let l = document.getElementsByClassName("biba");
+// console.log(l);
+a.setAttribute("class" , "n")
